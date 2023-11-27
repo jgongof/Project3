1c749a8330865f2d4a73c73fdc52a29a4a01f71c
hhussa31 <118236541+hhussa31@users.noreply.github.com>
2023-11-26 23:09:40 -0600

    Update ServerGUI.java

--- Project3/Server/src/main/java/ServerGUI.java
+++ Project3/Server/src/main/java/ServerGUI.java
@@ -1,21 +1,3 @@
-import java.util.HashMap;
-import java.util.ArrayList;
-
-import javafx.application.Application;
-import javafx.application.Platform;
-
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
@@ -129,4 +111,5 @@ public class ServerGUI extends Application{
         }
 
 }
+
 
